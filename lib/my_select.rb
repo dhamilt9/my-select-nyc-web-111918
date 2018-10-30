7c053215565fe538aacd769e3097cf391cb6286d
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-30 21:38:30 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,8 @@
 def my_select(collection)
  i=0
  collect=[]
- while i<collection.size
-   collect<<yield collection[i]
+ while i<collection.length
+   collect<<yield( )collection[i])
    i+=1
  end
  collection
